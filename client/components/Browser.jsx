a92381a3a73e0d17fcf4e3fe68df55fae769d89e
Johnny <jvsainsbury@gmail.com>
2021-04-23 11:33:59 +1200

    Add toolbar icons

--- client/components/Browser.jsx
+++ client/components/Browser.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Search from './Search'
+import Tools from './Tools'
 
 const Browser = () => {
   return (
     <>
       <div className="flex block md:w-auto md:h-auto bg-gray-100">
         <Search />
+        <Tools />
       </div>
     </>
   )
---
+++ client/components/Tools.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Tools = () => {
+  return (
+    <>
+      <img src='/images/icons/back-button.png'></img>
+      <img src='/images/icons/fast-forward.png'></img>
+      <img src='/images/icons/refresh.png'></img>
+      <img src='/images/icons/house.png'></img>
+
+    </>
+  )
+}
+
+export default Tools
