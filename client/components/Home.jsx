a108f9498c65051e4cdf3cea67544f60624300a3
Johnny <jvsainsbury@gmail.com>
2021-04-23 10:33:34 +1200

    Add Tailwind install upgrades

---
+++ client/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className="bg-yellow-400">
+        <h2>Menu:</h2>
+      </div>
+    </>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: ['./server/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
